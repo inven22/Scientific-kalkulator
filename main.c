6eb181dbcc97c2bdd573d142a01bc75ca316f215
ahmad al fajri <invenelektro@gmail.com>
2023-02-06 11:29:09 +0700

    main baru

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int main(){
-	
-	
+int main()	{
+	int opcion;
+	char temp[10];
+	do	{
+			printf("--------------------------------------------------------------");
+			printf("\n ===========>    Scientific Calculator  <==================\n");
+		    printf("==============================================================");
+		    printf("\n\n\n");
+			printf("1).test\n");
+			fgets(temp,10,stdin);
+			opcion  = atoi(temp);
+			switch(opcion)	{
+				case 1:
+				system("pause");
+				break;
+				case 2:
+				   system("pause");
+				break;
+				case 3:
+				system("pause");
+				break;
+				case 4:
+				system("pause");
+				break;
+				default:
+			   	system("pause");
+				break;
+			}
+	}while(opcion != 4);
+	return 0;
 }
