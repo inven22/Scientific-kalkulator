dacce889902e98c88ed4324c22cbc4db4edfe2bb
ahmad al fajri <invenelektro@gmail.com>
2023-02-24 15:36:02 +0700

    Update logaritma bebas ya guys

--- moduloperasiAhmad.h
+++ moduloperasiAhmad.h
@@ -1,34 +1,49 @@
+#ifndef moduloperasiAhmad_h
+#define moduloperasiAhmad_h
+
+int loga(int number);
+void tampil();
+int log_a_to_base_b(int a, int b);
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-double loga(double number) 
+int loga(int number) 
 {
 	return log10(number);
 }
-void tampillg()
+
+//void tampillg()
+//{
+//	double angka = 0, hasil;
+//	printf("Masukkan angka : ");
+//	scanf("%lf", &angka);
+//	hasil=loga(angka);
+//	printf("hasil log =%.2lf", hasil);
+//}
+int log_a_to_base_b(int a, int b)
 {
-	double angka = 0,hasil;
-	printf("Masukkan angka : ");
-	scanf("%lf", &angka);
-	hasil=loga(angka);
-	printf("hasil log =%.2lf",hasil);
+    return log2(a) / log2(b);
+}
+
+void tampil(){
+	float hasil, a,b;
+	a=4;
+	b=2;
+	hasil = log_a_to_base_b(a,b);
+	printf("%f",hasil);
 }
 
 
+//double naturalloga(double number)
+//{
+//	return log(number);
+//}
+//
+//void resultloga(double result)
+//{
+//  printf("Hasil : %lf",result);
+//}
 
-double naturalloga(double number)
-{
-	return log(number);
-}
-
-void resultloga(double result)
-{
-  printf("Hasil : %lf",result);
-}
-
-int main(){
-	tampillg();
-	
-	return 0;
-}
