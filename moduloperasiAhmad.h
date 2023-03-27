39d3066cbce9741bcbd83de594db716236da5465
ahmad al fajri <invenelektro@gmail.com>
2023-03-27 09:09:20 +0700

    log new

--- moduloperasiAhmad.h
+++ moduloperasiAhmad.h
@@ -1,71 +1,63 @@
 #ifndef moduloperasiAhmad_h
 #define moduloperasiAhmad_h
 
-int loga(int number);
+float loga(double number);
 void tampil();
-int log_a_to_base_b(int a, int b);
+float log_a_to_base_b(float a, float b);
 #endif
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-int loga(int number) 
+float loga(double number)
 {
-	return log10(number);
-}
-
-
-//void tampillg()
-//{
-//	double angka = 0, hasil;
-//	printf("Masukkan angka : ");
-//	scanf("%lf", &angka);
-//	hasil=loga(angka);
-//	printf("hasil log =%.2lf", hasil);
-//}
-int log_a_to_base_b(int a, int b)
+double hasil_log = 0.0;
+while (number >= 50.0)
 {
-    return log2(a) / log2(b);
+    number /= 10.0;
+    hasil_log += 1.0;
 }
 
-void tampil(){
-	float hasil, a,b;
-	a=4;
-	b=2;
-	hasil = log_a_to_base_b(a,b);
-	printf("%f",hasil);
+while (number < 1.0)
+{
+    number *= 50.0;
+    hasil_log -= 1.0;
 }
 
+double y = number - 1.0;
+double y_pow = y;
+double t = y;
+int i = 1;
 
-//int naturalloga(double number)
-//{
-//	return log(number);
-//}
-//
-//void resultloga(double result)
-//{
-//  printf("Hasil : %lf",result);
-//}
+while (i < 50)
+{
+    y_pow *= y;
+    t += ((i % 2 == 0) ? -1.0 : 1.0) * y_pow / (i + 1);
+    i++;
+}
 
+hasil_log += t;
 
-//void tampillg()
-//{
-//	double angka = 0, hasil;
-//	printf("Masukkan angka : ");
-//	scanf("%lf", &angka);
-//	hasil=loga(angka);
-//	printf("hasil log =%.2lf", hasil);
-//}
+return (float)hasil_log;
+}
 
+float log_a_to_base_b(float a, float b)
+{
+    float count = 0;
+    while (a >= b)
+    {
+        a /= b;
+        count++;
+    }
+    return count;
+}
 
+void tampil()
+{
+    float hasil, a, b;
+    a = 4;
+    b = 2;
+    hasil = log_a_to_base_b(a, b);
+    printf("%.2f,", hasil); // menggunakan koma sebagai separator desimal dan dua digit desimal setelah koma
+}
 
-//int naturalloga(double number)
-//{
-//	return log(number);
-//}
-//
-//void resultloga(double result)
-//{
-//  printf("Hasil : %lf",result);
-//}
